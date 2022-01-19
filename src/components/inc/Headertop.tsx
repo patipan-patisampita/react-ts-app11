373db84e3a4b0c17f90e71a2580883ad18d8ede1
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-19 13:57:06 +0700

    update

---
+++ src/components/inc/Headertop.tsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+
+class Headertop extends Component{
+    render(){
+        return(
+            <div>
+                <div className="container shadow-lg p-2 mt-3 mb-2 rounded">
+                    <div className="shadow-sm p-3 rounded">
+                        <div className="row">
+
+                            <div className="col-md-1">
+                                <div style={{width:'8rem'}}>
+                                    <img src={require('../images/thanos.png')} className="card-img-top" alt="img" />
+                                </div>
+                            </div>
+
+                            <div className="col-md-11">
+                                <div style={{width:'18rem'}}>
+                                    <h4>วิทยาลัยเทคนิคตราด</h4>
+                                    <h4>Trat Technical College</h4>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Headertop;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Headertop from "../inc/Headertop";
+
 
 const Home = () => {
     return (
         <div>
+            <Headertop />
             Welcome to Home Page
         </div>
     )
